76fe2170058f3cbf8419d5b1f1da4bded605b261
Marc Becker <marcbecker@posteo.de>
2020-10-15 22:48:42 +0200

    use google-chrome

--- R/print_pdf.R
+++ R/print_pdf.R
@@ -8,7 +8,7 @@ print_pdf = function(input, chrome = TRUE) {
   )
 
   dir.create("pdf", showWarnings = FALSE)
-  command = paste0(ifelse(chrome, "chrome", "chromium"), " --headless --print-to-pdf='pdf/mlr3.pdf' --virtual-time-budget=20000  --disable-gpu --run-all-compositor-stages-before-draw ", svr$url)
+  command = paste0(ifelse(chrome, "google-chrome", "chromium"), " --headless --print-to-pdf='pdf/mlr3.pdf' --virtual-time-budget=20000  --disable-gpu --run-all-compositor-stages-before-draw ", svr$url)
   print(command)
   system(command, wait = FALSE)
 }
